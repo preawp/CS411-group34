cf9fdcb6507c9cc89d69b928efd3421726818997
Axel Almaguer <axel.0619@hotmail.com>
2023-11-23 13:33:50 -0500

    Added part of OAuth

--- recipe-generator/src/App.js
+++ recipe-generator/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Header from './Components/Header';
 import IngredientSelection from './Components/IngredientSelection';
 import LoginPage from './Components/LoginPage';
+import { OAuthProvider, OAuthConsumer } from 'react-oauth';
+
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -13,14 +15,42 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <Header />
-      {isLoggedIn ? (
-        <IngredientSelection />
-      ) : (
-        <LoginPage onSignIn={handleSignIn} />
-      )}
-    </div>
+    <OAuthProvider
+      oauthKey="your_github_client_id"
+      oauthSecret="your_github_client_secret"
+      redirectUri="your_redirect_uri"
+    >
+      <div className="App">
+        {/* Use OAuthConsumer to wrap the components that depend on user authentication */}
+        <OAuthConsumer>
+          {({ loading, error, data, login }) => {
+            if (loading) return <p>Loading...</p>; // Display a loading message while OAuth is in progress
+            if (error) return <p>Error: {error.message}</p>; // Display an error message if OAuth encounters an error
+
+            if (data) {
+              // User is logged in
+              return (
+                // Render the component for authenticated users
+                <IngredientSelection />
+              );
+            } else {
+              // User is not logged in
+              return (
+                // Render the component for non-authenticated users, passing the handleSignIn function as a prop
+                <LoginPage onSignIn={handleSignIn} />
+              );
+            }
+          }}
+        </OAuthConsumer>
+
+        {/* <Header />
+        {isLoggedIn ? (
+          <IngredientSelection />
+        ) : (
+          <LoginPage onSignIn={handleSignIn} />
+        )} */}
+      </div>
+    </OAuthProvider>
   );
 }
 
